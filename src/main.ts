ec75776e624192edcaac7a07b688ddb209d83caf
João Vitor <joaovitorrods089@gmail.com>
2024-05-29 16:53:58 -0300

    feat: Jogo finalizado

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Actor, CollisionType, Color, Direction, Engine, Text, Follow, HiddenEvent, Random, vec, Font } from "excalibur"
-
+import { Actor, CollisionType, Color, Direction, Engine, Text, Follow, HiddenEvent, Random, vec, Font, Label, FontUnit, Sound, Loader } from "excalibur"
+import { delay } from "excalibur/build/dist/Util/Util"
 
 // 1 - Criar uma instancia de engine
 
@@ -27,18 +27,27 @@ game.input.pointers.primary.on("move", (event) => {
 	barra.pos.x = event.worldPos.x
 })
 
+let xx = 100
+let yy = 200
 
 //  4 - Criar Actor Bolinha
 const bolinha = new Actor
 ({
-	x: 100,
-	y: 200,
+	x: Math.trunc(Math.random() * game.drawWidth),
+	y: Math.trunc(Math.random() * game.drawHeight - 70),
 	radius: 10,
 	color: Color.Red
 })
 bolinha.body.collisionType = CollisionType.Passive
 // 5 - Criar movimentação da bolinha
 const VelocidadeBolinha = vec	(150, 150)
+
+
+let coresBolinha = [Color.Black, Color.Transparent, Color.Cyan, Color.Green, Color.Magenta, Color.Orange, Color.Red, Color.Rose, Color.White, Color.Yellow]
+
+let numCores = coresBolinha.length
+
+
 setTimeout( () => 
 {
 	bolinha.vel = VelocidadeBolinha
@@ -47,22 +56,33 @@ setTimeout( () =>
 bolinha.on("postupdate", () => {
 	// Se a bolinha colidir com o lado esquerdo
 	if (bolinha.pos.x < bolinha.width / 2){
-		bolinha.vel.x = VelocidadeBolinha.x * 2
+		bolinha.vel.x = VelocidadeBolinha.x * 5
 	}
 
 	// Se a bolinha colidir com o lado direito
 	if (bolinha.pos.x + bolinha.width / 2 > game.drawWidth){
-		bolinha.vel.x = VelocidadeBolinha.x * -2
+		bolinha.vel.x = VelocidadeBolinha.x * -5
 	}
 
 	// Se a bolinha colidir com o topo
 	if (bolinha.pos.y < bolinha.height / 2){
-		bolinha.vel.y = VelocidadeBolinha.y * 2
+		bolinha.vel.y = VelocidadeBolinha.y * 5
 	}
 	
 })
 
 game.add(bolinha)
+const somHit = new Sound  ('./audios/bolinha.wav');
+const loader = new Loader([somHit]);
+
+const somBarra = new Sound ('./audios/tenis.wav');
+const loaderB = new Loader ([somBarra])
+
+const somMorte = new Sound ('./audios/morte.mp3');
+const loaderC =new Loader ([somMorte])
+
+const somV = new Sound ('./audios/win.wav')
+const loaderD = new Loader ([somV])
 
 
 //  7 - Criar os blocos
@@ -107,20 +127,32 @@ listaBlocos.forEach	( bloco => {
 	game.add(bloco)
 })
 
-let pontos: 0
-
-const textoPontos = new Text({
-	text: "Hello World",
-	font: new Font ({size: 20})
+let pontos =  0
+const textoPontos = new Label({
+	text: pontos.toString(),
+	font: new Font({
+		size: 40,
+		color: Color.White,
+		strokeColor: Color.Black,
+		unit: FontUnit.Px
+	}),
+	pos: vec(600, 500)
 })
-const objetoTexto = new Actor ({
-	x: game.drawWidth - 80,
-	y: game.drawHeight - 15	
-})
 
-objetoTexto.graphics.use(textoPontos)
+game.add(textoPontos)
 
-game.add(objetoTexto)
+// const textoPontos = new Text({
+// 	text: "Hello World",
+// 	font: new Font ({size: 20})
+// })
+// const objetoTexto = new Actor ({
+// 	x: game.drawWidth - 80,
+// 	y: game.drawHeight - 15	
+// })
+
+// objetoTexto.graphics.use(textoPontos)
+
+// game.add(objetoTexto)
 
 
 let colidindo: boolean = false
@@ -131,7 +163,17 @@ bolinha.on("collisionstart",  (event) => {
 	if (listaBlocos.includes(event.other)) {
 		// Destruir bloco colidido
 		event.other.kill()
-		
+		pontos++
+		textoPontos.text = pontos.toString()
+		somHit.play(1.0)
+		if (pontos >= 15)
+	{
+		somV.play(1.0)
+		delay(2500)
+		alert("Você venceu")
+		window.location.reload()
+	}
+	bolinha.color = coresBolinha[Math.trunc (Math.random() * numCores)]
 	}
 
 
@@ -150,10 +192,20 @@ bolinha.on("collisionstart",  (event) => {
 
 bolinha.on("collisionend", () => {
 	colidindo  = false
+	somBarra.play(1.5)
 })
 
 bolinha.on("exitviewport", () => {
+	somMorte.play(1.0)
+	delay(1000)
 	alert("E morreu")
 	window.location.reload()
 })
-game.start()
+
+
+
+
+game.start(loader)
+game.start(loaderB)
+game.start(loaderC)
+game.start(loaderD)
